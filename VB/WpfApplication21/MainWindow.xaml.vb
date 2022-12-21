54232f3f75e083c8ad9ce4faf1ec6b67ed9cacf8
DevExpressExampleBot <fakeEmail>
2022-12-21 16:46:22 +0400

    Source auto update [skip ci]

--- VB/WpfApplication21/MainWindow.xaml.vb
+++ VB/WpfApplication21/MainWindow.xaml.vb
@@ -1,207 +1,209 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
 Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication21
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        Private employees_Renamed As List(Of Employee)
-        Private ReadOnly Property Employees() As List(Of Employee)
-            Get
-                If employees_Renamed Is Nothing Then
-                    PopulateEmployees()
-                End If
-                Return employees_Renamed
+        Private employeesField As List(Of Employee)
 
+        Private ReadOnly Property Employees As List(Of Employee)
+            Get
+                If employeesField Is Nothing Then PopulateEmployees()
+                Return employeesField
             End Get
         End Property
+
         Private Sub PopulateEmployees()
-            employees_Renamed = New List(Of Employee)()
-            employees_Renamed.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
-            employees_Renamed(0).Orders.Add(New Order("Supplier 1", Date.Now, "TV", 20))
-            employees_Renamed(0).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(3), "Projector", 15))
-            employees_Renamed(0).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(3), "HDMI Cable", 50))
-            employees_Renamed.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
-            employees_Renamed(1).Orders.Add(New Order("Supplier 1", Date.Now.AddDays(1), "Blu-Ray Player", 10))
-            employees_Renamed(1).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(1), "Blu-Ray Player", 10))
-            employees_Renamed(1).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(1), "Blu-Ray Player", 10))
-            employees_Renamed(1).Orders.Add(New Order("Supplier 4", Date.Now.AddDays(1), "Blu-Ray Player", 10))
-            employees_Renamed.Add(New Employee("Jack", "Lee", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Jack is a member of Toastmasters International."))
-            employees_Renamed(2).Orders.Add(New Order("Supplier 1", Date.Now, "AV Receiver", 20))
-            employees_Renamed(2).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(3), "Projector", 15))
-            employees_Renamed.Add(New Employee("Cindy", "Johnson", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
+            employeesField = New List(Of Employee)()
+            employeesField.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
+            employeesField(0).Orders.Add(New Order("Supplier 1", Date.Now, "TV", 20))
+            employeesField(0).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(3), "Projector", 15))
+            employeesField(0).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(3), "HDMI Cable", 50))
+            employeesField.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
+            employeesField(1).Orders.Add(New Order("Supplier 1", Date.Now.AddDays(1), "Blu-Ray Player", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(1), "Blu-Ray Player", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(1), "Blu-Ray Player", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 4", Date.Now.AddDays(1), "Blu-Ray Player", 10))
+            employeesField.Add(New Employee("Jack", "Lee", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Jack is a member of Toastmasters International."))
+            employeesField(2).Orders.Add(New Order("Supplier 1", Date.Now, "AV Receiver", 20))
+            employeesField(2).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(3), "Projector", 15))
+            employeesField.Add(New Employee("Cindy", "Johnson", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
         End Sub
 
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            gridControl1.ItemsSource = Employees
-
+            Me.gridControl1.ItemsSource = Employees
         End Sub
 
-        #Region "#MasterRowExpanded"
+'#Region "#MasterRowExpanded"
         Private Sub TableView_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
             Dim view As TableView = TryCast(sender, TableView)
-
             ' Avoid key processing when focus is within detail views
             ' or when a groupo row is focused
-            If Not view.IsFocusedView OrElse view.FocusedRowHandle < 0 Then
-                Return
-            End If
-
+            If Not view.IsFocusedView OrElse view.FocusedRowHandle < 0 Then Return
             ' Process CTRL+* key combination
-            If e.Key = Key.Multiply AndAlso ((Keyboard.Modifiers And ModifierKeys.Control) = ModifierKeys.Control) Then
+            If e.Key = Key.Multiply AndAlso (Keyboard.Modifiers And ModifierKeys.Control) = ModifierKeys.Control Then
                 Dim finalExpandedState As Boolean = Not view.Grid.IsMasterRowExpanded(view.FocusedRowHandle)
                 view.Grid.SetMasterRowExpanded(view.FocusedRowHandle, finalExpandedState)
                 e.Handled = True
             End If
         End Sub
-        #End Region ' #MasterRowExpanded
 
-        #Region "#ExpandCollapse"
+'#End Region  ' #MasterRowExpanded
+'#Region "#ExpandCollapse"
         Private Sub buttonExpandAll_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            For i As Integer = 0 To gridControl1.VisibleRowCount - 1
-                Dim rowHandle = gridControl1.GetRowHandleByVisibleIndex(i)
-                gridControl1.ExpandMasterRow(rowHandle)
-            Next i
+            For i As Integer = 0 To Me.gridControl1.VisibleRowCount - 1
+                Dim rowHandle = Me.gridControl1.GetRowHandleByVisibleIndex(i)
+                Me.gridControl1.ExpandMasterRow(rowHandle)
+            Next
         End Sub
+
         Private Sub buttonCollapseAllButThis_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            If tableView1.FocusedRowHandle >= 0 Then
-                gridControl1.ExpandMasterRow(tableView1.FocusedRowHandle)
-            End If
-            For i As Integer = 0 To gridControl1.VisibleRowCount - 1
-                Dim rowHandle As Integer = gridControl1.GetRowHandleByVisibleIndex(i)
-                If rowHandle <> tableView1.FocusedRowHandle Then
-                    gridControl1.CollapseMasterRow(rowHandle)
-                End If
-            Next i
+            If Me.tableView1.FocusedRowHandle >= 0 Then Me.gridControl1.ExpandMasterRow(Me.tableView1.FocusedRowHandle)
+            For i As Integer = 0 To Me.gridControl1.VisibleRowCount - 1
+                Dim rowHandle As Integer = Me.gridControl1.GetRowHandleByVisibleIndex(i)
+                If rowHandle <> Me.tableView1.FocusedRowHandle Then Me.gridControl1.CollapseMasterRow(rowHandle)
+            Next
         End Sub
-        #End Region ' #ExpandCollapse
+'#End Region  ' #ExpandCollapse
     End Class
 
     Public Class Employee
 
-        Private firstName_Renamed As String
+        Private firstNameField As String
 
-        Private lastName_Renamed As String
+        Private lastNameField As String
 
-        Private title_Renamed As String
+        Private titleField As String
 
-        Private notes_Renamed As String
+        Private notesField As String
 
-        Private orders_Renamed As List(Of Order)
+        Private ordersField As List(Of Order)
+
         Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal title As String, ByVal notes As String)
-            Me.firstName_Renamed = firstName
-            Me.lastName_Renamed = lastName
-            Me.title_Renamed = title
-            Me.notes_Renamed = notes
-            Me.orders_Renamed = New List(Of Order)()
+            firstNameField = firstName
+            lastNameField = lastName
+            titleField = title
+            notesField = notes
+            ordersField = New List(Of Order)()
         End Sub
+
         Public Sub New()
         End Sub
-        Public Property FirstName() As String
+
+        Public Property FirstName As String
             Get
-                Return firstName_Renamed
+                Return firstNameField
             End Get
+
             Set(ByVal value As String)
-                firstName_Renamed = value
+                firstNameField = value
             End Set
         End Property
-        Public Property LastName() As String
+
+        Public Property LastName As String
             Get
-                Return lastName_Renamed
+                Return lastNameField
             End Get
+
             Set(ByVal value As String)
-                lastName_Renamed = value
+                lastNameField = value
             End Set
         End Property
-        Public Property Title() As String
+
+        Public Property Title As String
             Get
-                Return title_Renamed
+                Return titleField
             End Get
+
             Set(ByVal value As String)
-                title_Renamed = value
+                titleField = value
             End Set
         End Property
-        Public Property Notes() As String
+
+        Public Property Notes As String
             Get
-                Return notes_Renamed
+                Return notesField
             End Get
+
             Set(ByVal value As String)
-                notes_Renamed = value
+                notesField = value
             End Set
         End Property
-        Public ReadOnly Property Orders() As List(Of Order)
+
+        Public ReadOnly Property Orders As List(Of Order)
             Get
-                Return orders_Renamed
+                Return ordersField
             End Get
         End Property
     End Class
 
     Public Class Order
 
-        Private supplier_Renamed As String
+        Private supplierField As String
 
-        Private date_Renamed As Date
+        Private dateField As Date
 
-        Private productName_Renamed As String
+        Private productNameField As String
 
-        Private quantity_Renamed As Integer
+        Private quantityField As Integer
+
         Public Sub New(ByVal supplier As String, ByVal [date] As Date, ByVal productName As String, ByVal quantity As Integer)
-            Me.supplier_Renamed = supplier
-            Me.date_Renamed = [date]
-            Me.productName_Renamed = productName
-            Me.quantity_Renamed = quantity
+            supplierField = supplier
+            dateField = [date]
+            productNameField = productName
+            quantityField = quantity
         End Sub
-        Public Property Supplier() As String
+
+        Public Property Supplier As String
             Get
-                Return supplier_Renamed
+                Return supplierField
             End Get
+
             Set(ByVal value As String)
-                supplier_Renamed = value
+                supplierField = value
             End Set
         End Property
-        Public Property [Date]() As Date
+
+        Public Property [Date] As Date
             Get
-                Return date_Renamed
+                Return dateField
             End Get
+
             Set(ByVal value As Date)
-                date_Renamed = value
+                dateField = value
             End Set
         End Property
-        Public Property ProductName() As String
+
+        Public Property ProductName As String
             Get
-                Return productName_Renamed
+                Return productNameField
             End Get
+
             Set(ByVal value As String)
-                productName_Renamed = value
+                productNameField = value
             End Set
         End Property
-        Public Property Quantity() As Integer
+
+        Public Property Quantity As Integer
             Get
-                Return quantity_Renamed
+                Return quantityField
             End Get
+
             Set(ByVal value As Integer)
-                quantity_Renamed = value
+                quantityField = value
             End Set
         End Property
     End Class
-
 End Namespace
